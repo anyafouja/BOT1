8e048a68438c80e9092e949500bca4ddd89d823b
anyafouja <anyafouja@gmail.com>
2026-06-14 21:22:33 +0700

    restore: workflow #40 version with yt-dlp fixes

--- cogs/music.py
+++ cogs/music.py
@@ -30,13 +30,12 @@ def _extract_info(url: str) -> dict:
     except subprocess.CalledProcessError as e:
         raise RuntimeError(e.stderr.strip() or str(e))
     lines = out.strip().split('\n')
-    json_line = next((l for l in lines if l.startswith('{')), lines[-1] if lines else '{}')
-    data = json.loads(json_line)
+    stream_url = lines[0] if lines else ''
+    json_str = next((l for l in reversed(lines) if l.startswith('{')), '{}')
+    data = json.loads(json_str)
     if 'entries' in data:
         data = data['entries'][0]
-    stream_url = lines[0] if lines and not lines[0].startswith('{') else None
-    if stream_url:
-        data['url'] = stream_url
+    data['url'] = stream_url
     return data
 
 
@@ -57,7 +56,7 @@ class YTDLSource(discord.PCMVolumeTransformer):
 
     @classmethod
     async def from_url(cls, url: str, volume=0.5):
-        data = await asyncio.get_event_loop().run_in_executor(None, lambda: _extract_info(url))
+        data = await asyncio.get_event_loop().run_in_executor(None, _extract_info, url)
         return await cls.from_data(data, volume=volume)
 
 
